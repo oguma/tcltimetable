31d4e5863c3789a4b490adffe9177519fa1d34d6
oguma <manabu.oguma@gmail.com>
2020-03-05 20:55:58 +0900

    Update timetable.tcl

--- timetable.tcl
+++ timetable.tcl
@@ -3,6 +3,19 @@ proc chime1 {} { exec play -q -V1 -v 0.2 chime1.ogg }
 proc chime2 {} { exec play -q -V1 -v 0.2 chime2.ogg }
 proc chime3 {} { exec play -q -V1 -v 0.5 chime3.wav }
 proc speak {msg} { exec espeak -s 140 $msg }
+proc dmsg {at1 at2} {
+  set t1 [clock scan $at1]
+  set t2 [clock scan $at2]
+  set delta [expr $t2 - $t1]
+  set dmin [expr $delta / 60]
+  set h [expr $dmin / 60]
+  set m [expr $dmin % 60]
+  set msg ""
+  if {$h == 1} { set msg [concat $h "hour"] }
+  if {$h > 1} { set msg [concat $h "hours"] }
+  if {$m != 0} { set msg [concat $msg [concat $m "minutes"]] }
+  return $msg
+}
 proc cond {at} {
   global now
   set t1 [clock format $now -format "%H:%M"]
@@ -11,7 +24,8 @@ proc cond {at} {
 }
 proc c {at} { if {[cond $at]} { chime1 }}
 proc s {at1 at2 msg} {
-  if {[cond $at1]} { chime2; speak $msg }
+  set msg2 [concat $msg "," [dmsg $at1 $at2]]
+  if {[cond $at1]} { chime2; speak $msg2 }
   if {[cond $at2]} { chime3 }
 }
 while {1} {
